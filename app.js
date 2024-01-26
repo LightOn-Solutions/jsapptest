1636b49aca8a39e556c3ae46e419c57ff16676df
LightOn-Solutions <46797270+LightOn-Solutions@users.noreply.github.com>
2024-01-26 11:25:52 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+const port = 80;
+app.listen(port, () => {
+    console.log(`Server is running on http://web.lighton.co.za:${port}`);
+});
